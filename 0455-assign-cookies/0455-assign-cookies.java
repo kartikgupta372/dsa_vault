bf7ca838704df88cbb7b0d4042ff6ce6f04aa5fa
Kartik Gupta <00guptakartik2006@gmail.com>
2026-07-23 22:29:48 +0530

    Time: 14 ms (92.13%), Space: 51.7 MB (6.49%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -1,25 +1,18 @@
-import java.util.*;
-
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
         Arrays.sort(g);
         Arrays.sort(s);
 
-        int i = 0;  
-        int j = 0; 
-        int count = 0;
+        int i = 0;
+        int j = 0;
 
         while (i < g.length && j < s.length) {
-
             if (s[j] >= g[i]) {
-                count++;
                 i++;
-                j++;
-            } else {
-                j++;
             }
+            j++;
         }
 
-        return count;
+        return i;
     }
 }
