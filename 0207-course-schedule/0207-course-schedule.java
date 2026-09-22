0d7ef205f2b354764da5252fa69406975844b85d
Kartik Gupta <00guptakartik2006@gmail.com>
2026-09-22 23:15:54 +0530

    Time: 4 ms (91.56%), Space: 47.2 MB (48.55%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> graph = new ArrayList<>();
+
+        for (int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int[] pre : prerequisites) {
+            graph.get(pre[1]).add(pre[0]);
+        }
+
+        int[] state = new int[numCourses]; 
+
+        for (int i = 0; i < numCourses; i++) {
+            if (state[i] == 0) {
+                if (hasCycle(graph, state, i)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private boolean hasCycle(List<List<Integer>> graph, int[] state, int node) {
+        if (state[node] == 1) return true; 
+        if (state[node] == 2) return false;
+
+        state[node] = 1;
+
+        for (int nei : graph.get(node)) {
+            if (hasCycle(graph, state, nei)) {
+                return true;
+            }
+        }
+
+        state[node] = 2;
+        return false;
+    }
+}
